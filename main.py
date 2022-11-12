96a68d428624a7f91e0d8eea54044e12c6654a61
Programmistochka <programmistochka@list.ru>
2022-11-12 18:03:10 +1200

    Add def average_grade_by_course

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Student:
         self.grades = {}
     
     def __str__(self):
-        res = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {self.middle_grade()}\nКурсы в процессе изучения: {", ".join(self.courses_in_progress)}\nЗавершенные курсы: {", ".join(self.finished_courses)}'
+        res = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {self.average_grade()}\nКурсы в процессе изучения: {", ".join(self.courses_in_progress)}\nЗавершенные курсы: {", ".join(self.finished_courses)}'
         return res 
 
     def rate_lecturer(self, lecturer, course, grade):
@@ -20,7 +20,7 @@ class Student:
         else:
             return 'Ошибка'
 
-    def middle_grade (self):
+    def average_grade (self):
         amount_grade = 0
         grades = 0
         for course in self.grades.keys():
@@ -36,7 +36,7 @@ class Student:
         if not isinstance(other, Student):
             print(f'Сравнение не возможно, {other} не относится к студентам')
         else:
-            return self.middle_grade() < other.middle_grade()
+            return self.average_grade() < other.average_grade()
 
 class Mentor:
     def __init__(self, name, surname):
@@ -52,10 +52,10 @@ class Lecturer(Mentor):
         self.grades = {}
     
     def __str__(self):
-        res = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.middle_grade()}'
+        res = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.average_grade()}'
         return res
     
-    def middle_grade (self):
+    def average_grade (self):
         amount_grade = 0
         grades = 0
         for course in self.grades.keys():
@@ -71,7 +71,7 @@ class Lecturer(Mentor):
         if not isinstance(other, Lecturer):
             print(f'Сравнение не возможно, {other} не относится к лекторам')
         else:
-            return self.middle_grade() < other.middle_grade()
+            return self.average_grade() < other.average_grade()
 
 
 class Reviewer(Mentor):
@@ -93,37 +93,43 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
-    
-
 def line(n=10):
     print()
     print('---'*n)
 
-   
+def average_grade_by_course(students, course):
+    grades_by_course = 0
+    amount_grade_by_course = 0
+    for stud in students:
+        print(stud)
+        for course_grades in stud['grades'].values():
+            print (course_grades)
+            if course in stud['courses_in_progress']:
+                print(course)
+                grades_by_course += sum(course_grades)
+                print(grades_by_course)
+                amount_grade_by_course += len(course_grades)
+    res = grades_by_course / amount_grade_by_course
+    return res
 
 stud1 = Student('Петр', 'Орлов', 'м')
 stud2 = Student('Нина', 'Радова', 'ж')
-stud3 = Student('Андрей', 'Кротов', 'м')
+
 lect1 = Lecturer('Николай', 'Иванович')
 lect2 = Lecturer('Виктория', 'Романовна')
-lect3 = Lecturer('Альберт', 'Семенович')
-lect4 = Lecturer('Ирина', 'Евгеньевна')
+
 rev1 = Reviewer('Алиса', 'Максимовна')
 rev2 = Reviewer('Михаил', 'Викторович')
-rev3 = Reviewer('Роман', 'Петрович')
 
-stud1.courses_in_progress += ['Математика', 'Русский язык', 'Английский язык', 'Аналитика']
+stud1.courses_in_progress += ['Математика', 'Русский язык']
 stud2.courses_in_progress += ['Английский язык', 'Аналитика']
 stud1.finished_courses = ['Литература']
 stud2.finished_courses = ['Математика']
 
 lect1.courses_atached += ['Математика', 'Аналитика']
 lect2.courses_atached += ['Русский язык']
-lect3.courses_atached += ['Английский язык']
-lect4.courses_atached += ['Литература']
 rev1.courses_atached += ['Русский язык']
 rev2.courses_atached += ['Английский язык']
-rev3.courses_atached += ['Математика, Аналитика']
 
 rev1.rate_hw(stud1,'Русский язык',3)
 rev1.rate_hw(stud1,'Русский язык',4)
@@ -136,28 +142,30 @@ rev2.rate_hw(stud2,'Английский язык',9)
 
 stud1.rate_lecturer(lect1, 'Математика', 2)
 stud1.rate_lecturer(lect1, 'Математика', 6)
-stud1.rate_lecturer(lect1, 'Аналитика', 6)
-stud1.rate_lecturer(lect2, 'Русский язык', 2)
+stud1.rate_lecturer(lect1, 'Аналитика', 6) #не должна попасть в расчет Аналитики нет в текущих курсах
+stud1.rate_lecturer(lect2, 'Русский язык', 8)
 stud1.rate_lecturer(lect2, 'Русский язык', 6)
-stud1.rate_lecturer(lect4, 'Литература', 6) #Не поставится, т.к. студент уже завершил этот курс
-stud2.rate_lecturer(lect3, 'Английский язык', 10)
 
-#print(f'Средний бал по оценкам: {middle_grade(lect1)}')
 print(rev1)
 line()
 print(stud1)
 line()
 print(stud2)
 line()
-print (stud1 < stud2)
+print (f'Сравнение студентов: {stud1.name} {stud1.surname} и {stud2.name} {stud2.surname}')
+print ('Результат: ', stud1 < stud2)
 line()
 print(lect1)
 line()
 print(lect2)
 line()
-print(lect3)
+print (f'Сравнение лекторов: {lect1.name} {lect1.surname} и {lect2.name} {lect2.surname}')
+print ('Результат: ', lect1 < lect2)
+
 line()
-print(lect4)
-line()
-print(lect1 > lect2)
-line()
+
+course = 'Математика'
+students = [{'name':'Васильев', 'surname':'Михаил', 'gender':'м', 'finished_courses':[], 'courses_in_progress':['Математика', 'Русский язык'], 'grades':{'Математика':[7,4,10,3,9], 'Русский язык':[3,5,2,5,4]}},
+           {'name':'Петров', 'surname':'Роман', 'gender':'м', 'finished_courses':['Математика'], 'courses_in_progress':['Русский язык'], 'grades':{'Русский язык':[9,5,7,5,4]}}]
+
+print(average_grade_by_course(students, course))
